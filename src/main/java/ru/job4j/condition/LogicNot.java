b3a9ccba0d7a0b1793df4720d36bb280e4ff926e
Alex1204MO <4109974@inbox.ru>
2023-08-13 14:36:13 +0300

    Логическое отрицание ! [#334072 #436581] лог

--- src/main/java/ru/job4j/condition/LogicNot.java
+++ src/main/java/ru/job4j/condition/LogicNot.java
@@ -4,15 +4,19 @@ public class LogicNot {
     public static boolean isEven(int num) {
         return num % 2 == 0;
     }
+
     public static boolean isPositive(int num) {
         return num > 0;
     }
+
     public static boolean notEven(int num) {
         return !isEven(num);
     }
+
     public static boolean notPositive(int num) {
         return !isPositive(num);
     }
+
     public static boolean notEvenAndPositive(int num) {
         return notEven(num) && isPositive(num);
     }
