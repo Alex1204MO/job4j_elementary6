11ea2ec082c576bdbcc280a5149cf1ae7d726e48
Alex1204MO <4109974@inbox.ru>
2023-08-13 10:57:40 +0300

    Сокращенный оператор && [#333783 #436468]3

--- src/main/java/ru/job4j/condition/AlertDivZero.java
+++ src/main/java/ru/job4j/condition/AlertDivZero.java
@@ -4,8 +4,7 @@ public class AlertDivZero {
     public static void possibleDiv(int number) {
         if (number == 0) {
             System.out.println("Could not div by 0.");
-        }
-        else if (number < 0) {
+        } else if (number < 0) {
             System.out.println("This is negative number");
         }
     }
