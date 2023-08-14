edd5b1d4022b90c984184cc14fb19f46e19db6e0
Alex1204MO <4109974@inbox.ru>
2023-08-14 07:29:09 +0300

    Switch [#334070 #436647] лог

--- src/main/java/ru/job4j/condition/SwitchWeek.java
+++ src/main/java/ru/job4j/condition/SwitchWeek.java
@@ -2,16 +2,34 @@ package ru.job4j.condition;
 
 public class SwitchWeek {
     public static String nameOfDay(int day) {
-        String name = switch (day) {
-            case 1 -> "Понедельник";
-            case 2 -> "Вторник";
-            case 3 -> "Среда";
-            case 4 -> "Четверг";
-            case 5 -> "Пятница";
-            case 6 -> "Суббота";
-            case 7 -> "Воскресенье";
-            default -> "Ошибка";
-        };
+        String name = "Tuesday";
+
+        switch (day) {
+            case 1:
+                name = "Monday";
+                break;
+            case 2:
+                name = "Tuesday";
+                break;
+            case 3:
+                name = "Wednesday";
+                break;
+            case 4:
+                name = "Thursday";
+                break;
+            case 5:
+                name = "Friday";
+                break;
+            case 6:
+                name = "Saturday";
+                break;
+            case 7:
+                name = "Sunday";
+                break;
+            default:
+                name = "ошибка";
+                break;
+        }
         return name;
     }
 
